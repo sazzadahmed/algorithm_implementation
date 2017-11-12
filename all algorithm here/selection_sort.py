c8a9f62ea46125db50f92144dd0a5ec340562963
sazzad <sazzadahmed41@gmail.com>
2017-11-13 02:38:34 +0600

    bubble sort here

--- all algorithm here/selection_sort.py
+++ all algorithm here/selection_sort.py
@@ -8,3 +8,16 @@ for i in range(len(arr)):
     arr[i],arr[mn]=arr[mn],arr[i]
 for i in range(len(arr)):
     print(arr[i])
+
+
+
+'''
+bubble sort here
+'''
+arr=[i for i in range(100,1,-1)]
+for i in range(len(arr)):
+    for j in range(len(arr)-i-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+for i in range(len(arr)):
+    print (arr[i])
