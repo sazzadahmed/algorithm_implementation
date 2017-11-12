db5774d82a09406d6fe40c975359b7edeeb1d8c6
sazzad <sazzadahmed41@gmail.com>
2017-11-13 02:03:00 +0600

    selection sort

---
+++ all algorithm here/selection_sort.py
@@ -0,0 +1,10 @@
+arr=[1,2,4,32,42323,4234,2,34,2,3,4,23,4,23,4]
+for i in range(len(arr)):
+    mn=i
+    for j in range(i,len(arr)):
+        if arr[j]<arr[mn]:
+            mn=j
+
+    arr[i],arr[mn]=arr[mn],arr[i]
+for i in range(len(arr)):
+    print(arr[i])
